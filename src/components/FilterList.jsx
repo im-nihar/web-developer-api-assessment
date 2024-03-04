0cd407de0211e39db8730f0dea70c651c58a44f8
im-nihar <niharsonawane@gmail.com>
2024-03-04 13:37:47 +0000

    [WIP] Changes in Filter component
    Working on dynamic filter changes.
    Implemented constant files for the filters and used in select labels.

--- src/components/FilterList.jsx
+++ src/components/FilterList.jsx
@@ -1,15 +1,14 @@
 import "./filterList.css";
 
-const FilterList = () => {
+const FilterList = ({ filterList, sortHeader }) => {
   return (
     <div>
       <div className="filter-container">
-        <label for="cars">Filter movies by: </label>
-        <select name="cars" id="cars">
-          <option value="volvo">Volvo</option>
-          <option value="saab">Saab</option>
-          <option value="mercedes">Mercedes</option>
-          <option value="audi">Audi</option>
+        <label>{sortHeader}: </label>
+        <select name="filters" id="filters" className="filters-select">
+          {filterList.map((item, index) => {
+            return <option value={item.value}>{item.name}</option>;
+          })}
         </select>
       </div>
     </div>
--- src/components/MovieFilter.jsx
+++ src/components/MovieFilter.jsx
@@ -2,15 +2,27 @@ import FilterList from "./FilterList";
 import "./movieFilter.css";
 
 const MovieFilter = () => {
+  const MOVIE_FILTER = [
+    { value: 1, name: "Release Date" },
+    { value: 2, name: "Rating" },
+  ];
+  const SORT_FILTER = [
+    { value: 3, name: "Alphabetical order" },
+    { value: 4, name: "Reverse alphabetical order" },
+  ];
+
+  const SORT_HEADERS_1="Filter movies by";
+  const SORT_HEADERS_2="Sort results";
+
   return (
     <div>
       <div className="filters-container">
         <div className="filter">
           <div>
-            <FilterList />
+            <FilterList filterList={MOVIE_FILTER} sortHeader={SORT_HEADERS_1}/>
           </div>
           <div>
-            <FilterList />
+            <FilterList filterList={SORT_FILTER}  sortHeader={SORT_HEADERS_2}/>
           </div>
         </div>
       </div>
